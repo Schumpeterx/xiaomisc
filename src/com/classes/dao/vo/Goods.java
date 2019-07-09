4a6445f2ddec4776ccf65c2667ecbad3e7808033
sqsx2019d5z <2436173309@qq.com>
2019-07-09 13:14:58 +0800

    login3

---
+++ src/com/classes/dao/vo/Goods.java
@@ -0,0 +1,40 @@
+package com.classes.dao.vo;
+
+public class Goods {
+private int gid;
+private String gname;
+private String gprice;
+private String gsign;
+private String gphoto;
+public int getGid() {
+	return gid;
+}
+public void setGid(int gid) {
+	this.gid = gid;
+}
+public String getGname() {
+	return gname;
+}
+public void setGname(String gname) {
+	this.gname = gname;
+}
+public String getGprice() {
+	return gprice;
+}
+public void setGprice(String gprice) {
+	this.gprice = gprice;
+}
+public String getGsign() {
+	return gsign;
+}
+public void setGsign(String gsign) {
+	this.gsign = gsign;
+}
+public String getGphoto() {
+	return gphoto;
+}
+public void setGphoto(String gphoto) {
+	this.gphoto = gphoto;
+}
+
+}
